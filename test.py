913a95003905a0b9b71d01562c3399c77d253ee1
gipyok <gipyok@gmail.com>
2021-03-27 01:19:32 -0700

    Ploted required coordinates

--- test.py
+++ test.py
@@ -27,19 +27,38 @@ def remove_noise_and_smooth(img):
     or_image = cv2.bitwise_or(img, closing)
     return or_image
 
-img = cv2.imread("Capture1.PNG")
-# img = img[280:320, 929:1095]
+# [y1:y2, x1:x2]
+# udi [280:320, 929:1095]
+# alliance [434:475 , 770:1085]
+# kills [548:710, 1140:1545]
+# t4 [612:647, 1377:1542]
+# t5 [662:694, 1195:1360]
+# total kills [457:486, 1250:1395]
+
+# 2nd picture
+# copy name (451, 184)
+# power [154:214 , 874:1162]
+# dead [533:593 ,1387:1587]
+
+
+img = cv2.imread("Capture.PNG")
+img = img[457:486, 1250:1395]
 # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # blur = cv2.GaussianBlur(img,(5,5),0)
 # img = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
 # kernel = np.ones((3,3),np.uint8)
 # img = cv2.erode(img,kernel,iterations = 1)
-d = pytesseract.image_to_data(img, output_type=Output.DICT)
-n_boxes = len(d['level'])
+
+# Perform OCR and save in data
+data = pytesseract.image_to_data(img, output_type=Output.DICT)
+# draw boxes around each data
+n_boxes = len(data['level'])
 for i in range(n_boxes):
-    (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+    (x, y, w, h) = (data['left'][i], data['top'][i], data['width'][i], data['height'][i])
     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-print(d["text"])
+print(data["text"])
+print("".join(data["text"]))
+
 cv2.imshow('img', img)
 cv2.waitKey(0)
