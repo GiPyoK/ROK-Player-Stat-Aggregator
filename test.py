380a7d9565d92cf14028d20f8fb10639f48a9984
gipyok <gipyok@gmail.com>
2021-06-14 17:46:12 -0700

    Updated OCR logic

--- test.py
+++ test.py
@@ -9,45 +9,48 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 def enhance_Image(image):
     # Grayscale, Gaussian blur, Otsu's threshold
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (3,3), 0)
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
     thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
     # Morph open to remove noise and invert image
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
     opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=1)
-    invert = 255 - opening
-    return thresh
-    
-def remove_noise_and_smooth(img):
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    filtered = cv2.adaptiveThreshold(img.astype(np.uint8), 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 41)
-    kernel = np.ones((1, 1), np.uint8)
+    return opening
+
+def enhance_Image2(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(image,(5,5),0)
+    image = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
+    kernel = np.ones((2,2),np.uint8)
+    image = cv2.erode(image,kernel,iterations = 1)
+    return image    
+
+def remove_noise_and_smooth(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    filtered = cv2.adaptiveThreshold(image.astype(np.uint8), 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 41)
+    kernel = np.ones((111, 111), np.uint8)
     opening = cv2.morphologyEx(filtered, cv2.MORPH_OPEN, kernel)
     closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
-    or_image = cv2.bitwise_or(img, closing)
+    or_image = cv2.bitwise_or(image, closing)
     return or_image
 
 # [y1:y2, x1:x2]
-# udi [280:320, 929:1095]
-# alliance [434:475 , 770:1085]
-# kills [548:710, 1140:1545]
-# t4 [612:647, 1377:1542]
-# t5 [662:694, 1195:1360]
-# total kills [457:486, 1250:1395]
+# udi [280:320, 935:1095]
+# alliance [434:475 , 770:921]
+# t4 [868:918, 1504:1713]
+# t5 [922:972, 1504:1713]
 
 # 2nd picture
 # copy name (451, 184)
 # power [154:214 , 874:1162]
-# dead [533:593 ,1387:1587]
+# dead troops [533:593 ,1387:1587]
 
 
-img = cv2.imread("Capture.PNG")
-img = img[457:486, 1250:1395]
-# img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# blur = cv2.GaussianBlur(img,(5,5),0)
-# img = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
-# kernel = np.ones((3,3),np.uint8)
-# img = cv2.erode(img,kernel,iterations = 1)
+img = cv2.imread("2-kills.PNG")
+img = img[280:320, 935:1095]
+# img = enhance_Image(img)
+# img = enhance_Image2(img)
+# img = remove_noise_and_smooth(img)
 
 # Perform OCR and save in data
 data = pytesseract.image_to_data(img, output_type=Output.DICT)
